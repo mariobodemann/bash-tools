2489f57363e95fb26cb13d8336cad3888028809a
Mario Bodemann <mario.bodemann@here.com>
2015-02-20 09:58:34 +0100

    escaping rainbows, one character at a time

--- rainbow.c
+++ rainbow.c
@@ -21,9 +21,11 @@ void rainbow (const char *word) {
 	int i = 0;
 	for ( i = 0; i < len; ++i) {
 		if (hsvToRgb( i * 360.0f / len, 1.0f, 1.0f, &r, &g, &b)) {
+			printf("\\[");
 			rgbf( r, g, b, FOREGROUND);
+			printf("\\]");
 			printf("%c", word[i]);
 		}
 	}
-	printf( "\e[m" );
+	printf("\e[m");
 }
